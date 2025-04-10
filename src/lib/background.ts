a06914ab1c148263d1db4ddcac848731a8053833
msa46 <mohamad.sanaye462@gmail.com>
2025-04-10 22:31:44 +0200

    Add mock code to fix the base build error

---
+++ src/lib/background.ts
@@ -0,0 +1,7 @@
+// Basic background script structure
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Extension installed');
+});
+
+export {};
+
---
+++ src/lib/content.ts
@@ -0,0 +1,5 @@
+// Basic content script structure
+console.log('Content script loaded');
+
+export {};
+
